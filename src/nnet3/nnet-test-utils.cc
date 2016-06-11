22c5e4dde5639d2ab0f51fdc86375dc07df97569
Tom Ko <tomkocse@gmail.com>
2016-06-11 06:30:12 -0400

    Fix the vectorization layout of the cudnn convolution component so now it has same bitwise results as the original convolution component in both forward and backward operations. Also the updated parameters are the same according to the same learning rate. Acceptance of yzx vectorization is added. However, the notation in vectorization should be changed as people usually arrange the letter of dimension from the largest to smallest stride.

--- src/nnet3/nnet-test-utils.cc
+++ src/nnet3/nnet-test-utils.cc
@@ -1249,6 +1249,7 @@ static void GenerateRandomGPUOnlyComponentConfig(std::string *component_type,
             filt_z_stride = (1 + Rand() % 2),
             num_filters = 1 + Rand() % 10,
             input_num_filters = 1 + Rand() % 10;
+
       os << " input-x-dim=" << input_x_dim
          << " input-y-dim=" << input_y_dim
          << " input-z-dim=" << input_z_dim
@@ -1281,11 +1282,13 @@ static void GenerateRandomCuDNNKaldiComponentPairConfig(std::string *component_t
       *component_type1 = "CuDNN3DConvolutionComponent";
       *component_type2 = "ConvolutionComponent";
 
+      int32 input_vectorization = Rand() % 2;
       std::string vectorization;
-      //hardcode here
-      vectorization = "zyx";
-      
-      // might need a better name and location for the matrix file
+      if (input_vectorization == 0) {
+        vectorization = "yzx";
+      } else  {
+        vectorization = "zyx";
+      }
       std::string matrix_filename="temp.mat";
 
       int32 input_x_dim = 10 + Rand() % 20,
@@ -1311,7 +1314,7 @@ static void GenerateRandomCuDNNKaldiComponentPairConfig(std::string *component_t
       int32 filter_dim = input_num_filters * filt_x_dim * filt_y_dim * filt_z_dim;
       mat.Resize(num_filters, filter_dim + 1);
       mat.SetRandn();
-     
+
       WriteKaldiObject(mat, matrix_filename, false);
 
       os1 << "input-x-dim=" << input_x_dim
@@ -1323,11 +1326,11 @@ static void GenerateRandomCuDNNKaldiComponentPairConfig(std::string *component_t
          << " filt-x-step=" << filt_x_step
          << " filt-y-step=" << filt_y_step
          << " filt-z-step=" << filt_z_step
-         << " input-num-filters=" << input_num_filters
+         << " input-vectorization-order=" << vectorization
          << " matrix=" << matrix_filename
          << " learning-rate=" << learning_rate;
 
-      os2 << " input-x-dim=" << input_x_dim
+      os2 << " input-x-dim=" << input_x_dim 
          << " input-y-dim=" << input_y_dim
          << " input-z-dim=" << input_z_dim
          << " filt-x-dim=" << filt_x_dim
